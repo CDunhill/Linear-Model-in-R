9f026990587a274d8d78c0ddc68e88e5012f30bb
Chris Dunhill <chrisdunhill@gmail.com>
2018-01-05 16:28:48 +0000

    tweak

--- Tutorial Section III (Program).R
+++ Tutorial Section III (Program).R
@@ -4,16 +4,18 @@
 # and go straight to Section III: Programming. This covers Pipes, Functions, Vectors and Iteration.
 # Section IV will be about the MODEL.
 
+
+# SECTION III Chapter 19: FUNCTIONS---------------------------------------------------------------------------------------------
+
 library (tidyverse)
 
+# ------------------------------------------------------------------------------------------------------------------------------
 
 # IF and ELSE IF conditions
 # Basic Syntax example...
 # if x==y true else false
-#
-# Can spread over
 
-x <- 1986595654475
+x <- 1986595654485
 
 if ((x/3)==round(x/3) & (x/5)==round(x/5)) {                 # Is X divisible by 3 AND 5?
   "FizzBuzz"
@@ -30,21 +32,41 @@ if ((x/3)==round(x/3) & (x/5)==round(x/5)) {                 # Is X divisible by
 # So where result is 0 'x' is divisible by 3
 
 
-# CREATING MY FIRST FUNCTIONs TO MAKE IT MORE REPEATABLE
+# CREATING MY FIRST FUNCTIONs TO MAKE IT MORE REPEATABLE -----------------------------------------------------------------------
 # The following checks for a 'fizzbuzz' and returns true if so...
 # Not the most amazing, but it works
 iz_buzz <- function (numb, div1, div2) {
   chk1 <- (numb/div1==round(numb/div1))
   chk2 <- (numb/div2==round(numb/div2))
-  all (chk1, chk2)
+  all (chk1, chk2)                                 # 'all': Given a set of logical vectors, are all of the values true?
 }
 
-# Now just need to make it do fizz, buzz and fizzbuzz...
-# the following does not work yet...
-f_b_fb <- function (numb, div1, div2) {
+# Now just need to make it do fizz, buzz and fizzbuzz --------------------------------------------------------------------------
+fizz_buzz <- function (numb, div1, div2) {
   chk1 <- (numb/div1==round(numb/div1))
   chk2 <- (numb/div2==round(numb/div2))
-  if all(chk1, chk2) == TRUE "FizzBuzz" elseif chk1 == TRUE "Fizz" elseif chk2 == TRUE "Buzz" else ""
+  chk3 <- chk1==chk2==TRUE
+  out <- ifelse (chk3,"FizzBuzz", ifelse (chk1,"Fizz", ifelse (chk2,"Buzz"
+      else {
+        "Nothing"
+      }
+    }
+
+
+
+# Syntax example...
+has_name <- function(x) {
+  nms <- names(x)
+  if (is.null(nms)) {
+    rep(FALSE, length(x))
+  } else {
+    !is.na(nms) & nms != ""
+  }
 }
+
+
+  
+
+
 
 
